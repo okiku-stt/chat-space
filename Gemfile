d9e4d3b1e9cd312744b5d5c2e36ac6f489e7a221
okiku-stt <59636022+okiku-stt@users.noreply.github.com>
2020-01-16 21:55:06 +0900

    setting-aws-s3

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ gem 'faker'
 group :production do
   gem 'unicorn', '5.4.1'
 end
+
+gem 'fog-aws'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   include CarrierWave::MiniMagick
   # storage :fog
   # process resize_to_fit: [200, 200]
