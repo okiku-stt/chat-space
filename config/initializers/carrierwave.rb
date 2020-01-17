f5dddf101e442cb338f3b2458423f5f21d46d6a7
okiku-stt <59636022+okiku-stt@users.noreply.github.com>
2020-01-17 17:00:14 +0900

    modify-carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'okiku-stt-chat2020'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/okiku-stt-chat2020'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
-
-  config.fog_directory  = 'okiku-stt-chat2020'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/okiku-stt-chat2020'
 end
